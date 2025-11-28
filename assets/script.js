9eb72ba65fe20873061960b459185eada85e2c4f
Abirock7 <109448849+Abirock7@users.noreply.github.com>
2025-11-27 19:42:57 -0600

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var typed = new Typed('#typed-text', {
+    strings: [
+        'SOC Analyst',
+        'Threat Detection',
+        'Incident Response',
+        'Blue Team Operations'
+    ],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true
+});
